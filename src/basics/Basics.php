d79dd3a561249c75640ec40f9cadcede444fd9d0
Ivan Sabat <sabotazh001@gmail.com>
2022-08-12 08:51:46 +0300

    Remove extra code that doesn't affect anything.

--- src/basics/Basics.php
+++ src/basics/Basics.php
@@ -70,8 +70,6 @@ class Basics implements BasicsInterface
     {
         $this->validator->isValidStringException($input);
 
-        $input = str_split($input); /* optional */
-
         return !boolval($input[0] + $input[1] + $input[2] - $input[3] - $input[4] - $input[5]);
     }
 }
